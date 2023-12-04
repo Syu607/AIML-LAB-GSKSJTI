2195449555c8ca60a5c83d30bb6487a12dbe01ac
Mohammed Sulaiman I <61232626+Syu607@users.noreply.github.com>
2023-12-04 17:48:51 +0530

    Update lab7.py

--- LAB-7/lab7.py
+++ LAB-7/lab7.py
@@ -1,51 +1,36 @@
 from sklearn.cluster import KMeans
-from sklearn import preprocessing
-from sklearn.mixture import GaussianMixture 
-from sklearn.datasets import load_iris
+from sklearn.mixture import GaussianMixture
+import sklearn.metrics as metrics
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-dataset = load_iris()
-print("\n IRIS Dataset:\n", dataset.data)
-print("\n IRIS Features:\n", dataset.feature_names) 
-print("\n IRIS Target:\n", dataset.target)
-print("\n IRIS Target:\n", dataset.target_names)
 
-X = pd.DataFrame(dataset.data) 
-X.columns=['Sepal_Length','Sepal_Width','Petal_Length','Petal_Width'] 
+dataset = pd.read_csv("iris.csv")
 
-y=pd.DataFrame(dataset.target)
-y.columns=['Targets']
+X = dataset.iloc[:, :-1]  
 
-print(y)
+label = {'Iris-setosa': 0,'Iris-versicolor': 1, 'Iris-virginica': 2} 
 
-plt.figure(figsize=(8,5))
-colormap=np.array(['red','lime','blue'])
+y = [label[c] for c in dataset.iloc[:, -1]]
 
-# Plotting without Clustering
-plt.subplot(1,3,1) 
-plt.scatter(X.Petal_Length,X.Petal_Width,c=colormap[y.Targets],s=20) 
-plt.title('Before Clustering')
+plt.figure(figsize=(14,7))
+colormap=np.array(['red','lime','black'])
 
-# Plotting with K-Means Clustering
+# REAL PLOT
+plt.subplot(1,3,1)
+plt.title('Real')
+plt.scatter(X.Petal_Length,X.Petal_Width,c=colormap[y])
+
+# K-PLOT
+model=KMeans(n_clusters=3, random_state=0).fit(X)
 plt.subplot(1,3,2)
-model = KMeans(n_clusters=3)
-model.fit(X) 
-predY = np.choose(model.labels_,[0,1,2]).astype(np.int64) 
-plt.scatter(X.Petal_Length,X.Petal_Width,c=colormap[predY],s=20) 
-plt.title('KMeans Clustering')
+plt.title('KMeans')
+plt.scatter(X.Petal_Length,X.Petal_Width,c=colormap[model.labels_])
 
-# Plotting with EM using GMM Clustering
-scaler=preprocessing.StandardScaler() 
-scaler.fit(X)
-xsa=scaler.transform(X) 
-xs=pd.DataFrame(xsa,columns=X.columns) 
-gmm=GaussianMixture(n_components=3) 
-gmm.fit(xs)
-
-y_cluster_gmm=gmm.predict(xs) 
+# GMM PLOT
+gmm=GaussianMixture(n_components=3, random_state=0).fit(X)
+y_cluster_gmm=gmm.predict(X)
 plt.subplot(1,3,3)
-plt.scatter(X.Petal_Length,X.Petal_Width,c=colormap[y_cluster_gmm],s=20) 
-plt.title('GMM with EM Clustering')
-plt.show();
+plt.title('GMM Classification')
+plt.scatter(X.Petal_Length,X.Petal_Width,c=colormap[y_cluster_gmm])
